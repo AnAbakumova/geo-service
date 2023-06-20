0fbb2492a8504d33b9d1e9b27b1e7b242d0cf3ff
AnAbakumova <miss.abackumowa@yandex.ru>
2023-06-20 19:54:55 +0300

    test added

---
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -0,0 +1,53 @@
+package ru.netology.geo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoServiceImplTest {
+
+    @Test
+    public void testByIpRussia(){
+        GeoService geoService = new GeoServiceImpl();
+
+        Country actual = geoService.byIp("172.").getCountry();
+        Country expected = new Location(null, Country.RUSSIA, null, 0).getCountry();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testByIpUSA(){
+        GeoService geoService = new GeoServiceImpl();
+
+        Country actual = geoService.byIp("96.").getCountry();
+        Country expected = new Location(null, Country.USA, null, 0).getCountry();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testByIpMoscow(){
+        GeoService geoService = new GeoServiceImpl();
+
+        Country actual = geoService.byIp("172.0.32.11").getCountry();
+        Country expected = new Location("Moscow", Country.RUSSIA, null, 0).getCountry();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testByIpNewYork(){
+        GeoService geoService = new GeoServiceImpl();
+
+        Country actual = geoService.byIp("96.44.183.149").getCountry();
+        Country expected = new Location("New York", Country.USA, null, 0).getCountry();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
